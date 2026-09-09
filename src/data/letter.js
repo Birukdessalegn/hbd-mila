87c850042696c20c836117b6d7c5be56a98fd6fd
Birukdessalegn <birukdessalegn123@gmail.com>
2026-09-09 21:15:35 +0300

    feat: add birthday letter content and SPA route redirects

--- src/data/letter.js
+++ src/data/letter.js
@@ -14,28 +14,27 @@ export const letterData = {
   paragraphs: [
     "Happy birthday, Mila. ❤️",
 
-    "I don't know if words will ever be enough to explain how much I love you, but I still want to try.",
+    "yene fkrr ychin ken mn yakl setebkat endenebere atawkim yene abeba mariamn.....beka gena kenun yenegershgn ken jemro new sasbat yeneberew.",
 
-    "I love you more than I can put into words. You have become such an important part of my life, and I honestly cannot imagine my days without you.",
+    "ena migermsh yane gena beka esua ga heje new makebrlat beye nebere kal yegebahut mariamn enatyee gn yaw anchi atchym akalew ykebdeshal yene gela gn tesaktolgn abresh hogne cake eyekoresn bakebrlsh mn yakll des ylegn endenebere mariamn yene fkr.",
 
-    "I love your smile. I love your laugh. I love the way you talk, the way you care, and even the little things you do that you may not even notice.",
+    "yeketay amet ldeteshn beka endih argen enakebrewalen yene tafach eshi abebaye yene fkr",
 
-    "Being with you makes me happy. Talking to you makes my day better. And even when we are far from each other, you are always in my heart and in my thoughts.",
+    "bezih nehase wer wst betam bezuu metfo yehonu gizewoch alfewal yene fkrr mehon yelelebachew negeroch alfewal awo akalehu....yene fkrr gn be adisu amet mariamn enatyee destegna honen endenenor new yene megnote enate yanchim endeza yhonal beye asbalew",
 
-    "You are not just my girlfriend. You are my best friend, my happiness, and one of the most beautiful parts of my life.",
+    "enate mnalbat anchim kafekershgn ena ketekebelshgn yene fkrr destegna honen eske tdar gizeachn dres abren enehun yene fkr?",
 
-    "I know I am not perfect. Sometimes I may make mistakes, sometimes I may not find the right words, but one thing I never want you to doubt is my love for you.",
+    "akalew betam mastela sew hognalew lk yelelegn sew hognalew enate nmabate larg yene alem....mulu sew adelehum gn anchin destegna lemareg ena mulu lemareg lnur enate fkejilgn",
 
-    "I want to keep making memories with you. I want to see you smile, support you when things are hard, celebrate your success, and be beside you through all the beautiful moments that are still waiting for us.",
+    "yhen zare yeserahut le sucks adelem gn memory endihon slemfelg new enate mastawesha yhonenal yenena anchi tru mastawesha ynorenal abebaye abren kehonen",
 
-    "On your birthday, I just want you to know how thankful I am that you are in my life.",
 
-    "I love you today, I will love you tomorrow, and I hope I get to love you for many more birthdays to come."
+    "anchin Mila beye yeterahush yene wud setota slehonsh new enate eshi.....hulem afekrshalew yene abeba hulemm"
   ],
 
   highlight: "Mila, you are deeply loved. You are my happiness, my favorite person, and my heart. ❤️",
 
-  signature: "With all my love,",
+  signature: "yanchi afkari,",
 
   author: "Biruk ❤️",
 
